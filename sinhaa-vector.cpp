cabaa352dd19510935a9b8d697fd05f6e72e3e28
Aditya Sinha <adityasinha347@gmail.com>
2026-03-24 13:14:14 +0530

    caacity related functions done in vector

--- sinhaa-vector.cpp
+++ sinhaa-vector.cpp
@@ -1,6 +1,99 @@
+#include <limits>
 #include "sinhaa-vector.h"
 
+const char* sinhaa::out_of_bounds_error::what () const noexcept {
+    return "sinhaa::vector: index out of bounds";
+}
+
 template <typename T>
-sinhaa::vector<T>::vector() {
+sinhaa::vector<T>::vector () {
+    capacity = 0;
+    length = 0;
+    data = nullptr;    
+}
+
+template <typename T>
+sinhaa::vector<T>::~vector () {
+    delete[] data;
+}
+
+template <typename T>
+T& sinhaa::vector<T>::operator[] (int index) {
+    if (index < 0 || index >= length) {
+        throw out_of_bounds_error();
+    }
+    return data[index];
+}
+
+template <typename T>
+std::size_t sinhaa::vector<T>::size() const {
+    return length;
+}
+
+template <typename T>
+std::size_t sinhaa::vector<T>::max_size() const {
+    return std::numeric_limits<std::size_t>::max() / sizeof(T);
+}
+
+template <typename T>
+void sinhaa::vector<T>::resize (size_t new_capacity) {
+    capacity = new_capacity;
+    T* new_data = new T[capacity];
+
+    for (int i = 0; i < length; i++)
+        new_data[i] = data[i];
     
+    for (int i = length; i < capacity; i++) {
+        new_data[i] = new T();
+    }
+
+    delete[] data;
+    data = new_data;
+}
+
+template <typename T>
+void sinhaa::vector<T>::resize (size_t new_capacity, T value) {
+    capacity = new_capacity;
+    T* new_data {new T {capacity}};
+
+    for (int i = 0; i < length; i++)
+        new_data[i] = data[i];
+
+    for (int i = length; i < capacity; i++)
+        new_data[i] = new T(value);
+
+    delete[] data;
+    data = new_data;
+}
+
+template <typename T>
+size_t sinhaa::vector<T>::capacity () const {
+    return capacity;
+}
+
+template <typename T>
+bool sinhaa::vector<T>::empty () const {
+    return length == 0;
+}
+
+template <typename T>
+void sinhaa::vector<T>::reserve (size_t min_capacity) {
+    if(capacity < min_capacity)
+        resize(min_capacity);
+}
+
+template <typename T>
+void sinhaa::vector<T>::shrink_to_fit () {
+    resize(length);
+}
+
+template <typename T>
+void sinhaa::vector<T>::push_back (int value) {
+    if(length == capacity) reallocate();
+    data[length++] = value;
+}
+
+template <typename T>
+void sinhaa::vector<T>::reallocate () {
+    resize(capacity * 2);
 }
--- sinhaa-vector.h
+++ sinhaa-vector.h
@@ -1,20 +1,38 @@
+#include <exception>
 #ifndef SINHAA_VECTOR_H
 #define SINHAA_VECTOR_H
 
-namespace sinhaa{
+namespace sinhaa {
+    
+    class out_of_bounds_error : public std::exception {
+    public:
+        const char* what() const noexcept override;
+    };
+    
     template <typename T>
-    class vector{
+    class vector {
         private:
-            int length {};
-            int capacity {};
+            size_t length {};
+            size_t capacity {};
             T* data {};
         
         public:
-            vector<T>();
-            T& operator[](int index);
-            void reallocate();
-            ~vector();
-            
+            vector<T> ();
+            ~vector ();
+            T& operator[] (int index);
+
+            std::size_t size() const;
+            std::size_t max_size() const;
+            void resize (size_t new_capacity);
+            void resize (size_t new_capacity, T value);
+            size_t capacity () const;
+            bool empty () const;
+            void reserve (size_t min_capacity);
+            void shrink_to_fit ();
+
+
+            void push_back (int value);
+            void reallocate ();
     };
 }
 
